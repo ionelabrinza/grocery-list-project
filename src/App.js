5b5ab7e73ade70d7db6b094d08470b43baba7920
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-06-16 22:02:38 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
+
+function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIdEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  return (
+    <section className="section-center">
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
