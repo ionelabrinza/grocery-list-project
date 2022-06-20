782cf7765da01159313a0242002fdd2f39868198
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-06-19 21:12:18 -0500

    clear items

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     } else if (name && isEditing) {
       //deal with edit
     } else {
-      // show alert
+      showAlert(true, 'success', 'item added to the list');
       const newItem = { if: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName('');
@@ -28,6 +28,10 @@ function App() {
   };
   const showAlert = (show = false, type = '', msg = '') => {
     setAlert({ show, type, msg });
+  };
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
   };
   return (
     <section className="section-center">
@@ -50,7 +54,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
